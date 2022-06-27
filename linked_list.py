28d6e7e7a54a9956ff116c8ffd38d99fddfaf24b
AiBiC-Data <76275193+AiBiC-Data@users.noreply.github.com>
2022-06-27 18:42:19 +0900

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,103 @@
+class Node:
+    """링크드 리스트의 노드 클래스"""
+
+    def __init__(self, data):
+        self.data = data  # 노드가 저장하는 데이터
+        self.next = None  # 다음 노드에 대한 레퍼런스
+
+
+# 데이터 2,3,5,7,11을 담는 노드를 생성
+head_node = Node(2)
+node_1 = Node(3)
+node_2 = Node(5)
+node_3 = Node(7)
+tail_node = Node(11)
+
+
+class LinkedList:
+    """링크드 리스트 클래스"""
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_after(self, previous_node, data):
+        """링크드 리스트 주어진 노드 뒤 삽입 연산 메소드"""
+        new_node = Node(data)
+
+        # 가장 마지막 순서 삽입
+        if previous_node is self.tail:
+            self.tail.new_node = new_node
+            self.tail = new_node
+        else:  # 두 노드 사이에 삽입
+            new_node.next = previous_node.next
+            previous_node.next = new_node
+
+    def find_node_at(self, index):
+        """링크드 리스트 접근 연산 메소드. 파라미터 인덱스는 항상 있다고 가정"""
+        iterator = self.head
+
+        for _ in range(index):
+            iterator = iterator.next
+
+        return iterator
+
+    def delete_after(self, previous_node):
+        """링크드 리스트 삭제 연산. 주어진 노드 뒤 노드를 삭제한다"""
+        data = previous_node.next.data
+        # 지우려는 노드가 tail 노드일때:
+        if previous_node.next is self.tail:
+            previous_node.next = None
+            self.taiil = previous_node
+            # 두 노드 사이 노드를 지울 때
+        else:
+            previous_node.next = previous_node.next.next
+        return data
+
+    def append(self, data):
+        """링크드 리스트 추가 연산 메소드"""
+        new_node = Node(data)
+
+        if self.head is None:  # 링크드 리스트가 비어 있는 경우
+            self.head = new_node
+            self.tail = new_node
+        else:  # 링크드 리스트가 비어 있지 않은 경우
+            self.tail.next = new_node
+            self.tail = new_node
+
+
+# 새로운 링크드 리스트 생성
+my_list = LinkedList()
+# 링크드 리스트에 데이터 추가
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+my_list.append(7)
+my_list.append(11)
+
+# 링크드 리스트 노드에 접근 (데이터 가지고 오기)
+print(my_list.find_node_at(3).data)
+
+# 링크듣 리스트 노드에 접근 (데이터 바꾸기)
+my_list.find_node_at(2).data = 13
+
+# 링크드 리스트 출력
+iterator = my_list.head
+
+while iterator is not None:
+    print(iterator.data)
+    iterator = iterator.next
+
+
+# 노드들을 연결
+head_node.next = node_1
+node_1.next = node_2
+node_2.next = node_3
+node_3.next = tail_node
+
+# 노드 순서대로 출력
+iterator = head_node
+
+while iterator is not None:
+    print(iterator.data)
+    iterator = iterator.next
